7e64a78a43d1635042be41b7b596e0a01883bef4
byniov1 <karol.nowak0490@protonmail.com>
2026-03-01 10:16:22 +0100

    feat: further work

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Concurrent;
+using System.Globalization;
 
+/*
 Console.WriteLine("Hello, World!");
+
+string myFriendName;
+
+myFriendName = "Janick";
+Console.WriteLine($"Hello, {myFriendName}");
+
+myFriendName = "Frank";
+Console.WriteLine($"Hello, {myFriendName}");
+
+string? input = Console.ReadLine();
+Console.WriteLine($"You entered: {input}");
+
+int myNumber = 0;
+Console.WriteLine("My number =  " + myNumber);
+*/
+
+// //zabugowane to troszkę xD
+// double? userNumber = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+// double? userNumber2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+// Console.WriteLine($"Addition of entered numbers is {userNumber + userNumber2}");
+
+//Implicit 
+// int myInt = 13;
+// double myDouble = myInt;
+// Console.WriteLine(myDouble);
+//
+// //Explicit
+// long myLong = 2000000000000000000;
+// int myInt2 = (int)myLong;
+// Console.WriteLine(myInt2);
+//
+//
+// string myBoolString = "true";
+// bool myBool = Convert.ToBoolean(myBoolString);
+
+
+var s = "11";
+var i = 11;
+var x = i + s;
+Console.WriteLine(x);
+
