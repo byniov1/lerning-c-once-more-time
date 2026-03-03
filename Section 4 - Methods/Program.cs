b1b15ba66edf93783f90c6b810b30ad57dba685c
byniov1 <karol.nowak0490@protonmail.com>
2026-03-03 07:40:42 +0100

    feat: ruther work section 4

---
+++ Section 4 - Methods/Program.cs
@@ -0,0 +1,188 @@
+﻿namespace Section_4___Methods;
+
+// class Program
+// {
+//     //Fields or instance variable
+//     public int myResult;
+//     
+//     static void Main(string[] args)
+//     {
+//         Program program = new Program();
+//         program.MyFirstMethod("Something passed to a method");
+//         // MyFirstMethod();
+//         program.myResult++;
+//
+//         // void MyFirstMethod()
+//         // {
+//         //     throw new NotImplementedException();
+//         // }
+//     }
+//     
+//     // static void MyFirstMethod(string firstParameter)
+//     void MyFirstMethod(string firstParameter)
+//     {
+//         Console.WriteLine($"My first void method that prints {firstParameter}");
+//     }
+//
+// }
+
+//WeatherStationSimulator
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of days to simulate");
+        int days = int.Parse(Console.ReadLine());
+
+        int[] temperature = new int[days];
+        string[] conditions = { "Suuny", "Rainy", "Cloudy", "Snowy" };
+        string[] weatherConditions = new string[days];
+        
+        Random rnd = new Random();
+
+        for (int i = 0; i < days; i++)
+        {
+            temperature[i] = rnd.Next(-10, 40);
+            weatherConditions[i] = conditions[rnd.Next(conditions.Length)];
+        }
+
+        double averageTemperature = CalculateAverageTemperature(temperature);
+        Console.WriteLine($"Average temperature: {averageTemperature}");
+
+        
+        // temperature.Min();
+        // temperature.Max();
+        Console.WriteLine($"Min temperature was: {temperature.Min()} \n Max temperature was: {temperature.Max()} ");
+        Console.WriteLine($"Min temperature was: {MinTemperature(temperature)}");
+        
+    }
+
+    private static double CalculateAverageTemperature(int[] temperature)
+    {
+        // double sum = 0;
+        // foreach (var temper in temperature)
+        // {
+        //     sum += temper;
+        // }
+        //Code after implementing suggestion
+        var sum = temperature.Aggregate<int, double>(0, (current, temper) => current + temper);
+        
+        var average = sum / temperature.Length;
+        return average;
+    }
+
+    static int MinTemperature(int[] temperature)
+    {
+        int min = temperature[0];
+
+        foreach (var temper in temperature)
+        {
+            if(temper < min)
+            {
+                min = temper;
+            }
+        }
+        
+        return min;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
