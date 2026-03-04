d88e6384f10ffcfe6f065a2af2e373ef6e9041ae
byniov1 <karol.nowak0490@protonmail.com>
2026-03-04 15:05:38 +0100

    feat: init section 6 collections

---
+++ Section 6 - Collection/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Section_6___Collection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> colors = new List<string>();
+        colors.Add("Red");
+        colors.Add("Blue");
+        colors.Add("Yellow");
+
+        foreach (string color in colors)
+        {
+            Console.WriteLine($"Color: {color}");
+        }
+    }
+}
