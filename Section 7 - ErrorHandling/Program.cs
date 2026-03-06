980e3a8196495d510a82209e3f97eeaaaa896afa
byniov1 <karol.nowak0490@protonmail.com>
2026-03-06 08:21:35 +0100

    feat: Section 7 - ErroHandling init

---
+++ Section 7 - ErrorHandling/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Section_7___ErrorHandling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        try
+        {
+            int num1 = 0;
+            int num2 = 2;
+            int result = num2 / num1;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            throw;
+        }
+            Console.WriteLine($"");
+        
+        Console.WriteLine(result);
+    }
+}
