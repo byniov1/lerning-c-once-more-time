aea733ea822f82c4a000700119131d8473239164
byniov1 <karol.nowak0490@protonmail.com>
2026-03-08 16:17:42 +0100

    feat: Section 9 - Interface - init

---
+++ Section 9 - Interface/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Section_9___Interface;
+
+public interface IAnimal
+{
+    void MakeSound();
+    void Eat(string food);
+    // string Eat(string food);
+
+}
+
+public class Dog : IAnimal
+{
+    public void MakeSound()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Eat(string food)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+    }
+}
