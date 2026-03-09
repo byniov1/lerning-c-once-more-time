d5b7921505d087a83c6a6b29e3128f46c4f6a59c
byniov1 <karol.nowak0490@protonmail.com>
2026-03-09 15:30:43 +0100

    feat: Section 10 - Sctructs

--- Section 10 - Structs/Program.cs
+++ Section 10 - Structs/Program.cs
@@ -2,11 +2,14 @@
 
 public struct Point
 {
+    //Jest częstą praktyką że structs robi się immutable by declaring all fields as readonly and providing only get accessors for props
     // public int X { get; set; }
     // public int Y { get; set; }
-    public int X;
-    public int Y;
-
+    // public int X;
+    // public int Y;
+    public double X { get;  }
+    public double Y { get; }
+    
     public Point(int x, int y)
     {
         X = x;
@@ -18,12 +21,21 @@ public struct Point
         Console.WriteLine($"Point: ({X}, {Y})");
         // Console.SetCursorPosition(X, Y);
     }
+
+    public double DistanceTo(Point other)
+    {
+        double dx = other.X - X;
+        double dy = other.Y - Y;
+        return Math.Sqrt(dx * dx + dy * dy);
+        // return Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
+    }
 }
 
 
 class Program
 {
-    static void Main(string[] args)
+    //1
+    /*static void Main(string[] args)
     {
         Console.Write("P0: ");
         Point p0 = new Point();
@@ -53,5 +65,14 @@ class Program
         Console.Write("P1: ");
         p1.Display();
 
+    }*/
+
+    private static void Main(string[] args)
+    {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
+        
+        double distance = p1.DistanceTo(p2);
+        Console.WriteLine($"Distance between points: {distance:F2}");
     }
 }
