39bfa59accf7b950af4bf060ceb42db9040a4a9d
byniov1 <karol.nowak0490@protonmail.com>
2026-03-09 10:13:48 +0100

    feat: Section 9 - Interface

--- Section 9 - Interface/Program.cs
+++ Section 9 - Interface/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace Section_9___Interface;
 
-public interface IAnimal
+//1
+/*public interface IAnimal
 {
     void MakeSound();
     void Eat(string food);
@@ -21,6 +22,19 @@ public class Dog : IAnimal
     }
 }
 
+public class Cat : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("Meao");
+    }
+
+    public void Eat(string food)
+    {
+        Console.WriteLine("Cat eat" + food);
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
@@ -28,5 +42,99 @@ class Program
         Dog dog = new Dog();
         dog.MakeSound();
         dog.Eat("Treat");
+        
+        Cat cat = new Cat();
+        cat.MakeSound();
+        cat.Eat("Treat");
+    }
+}*/
+
+//2
+/*public class Animal
+{
+    public virtual void MakeSound()
+    {
+        //Hmm to jest fajne 
+        Console.WriteLine($"{this.GetType().Name} MakeSound");
+        Console.WriteLine("Some generic animal sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine($"Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine($"Meow");
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal dog = new Dog();
+        dog.MakeSound();
+    }
+}*/
+
+public interface IPaymentProcessor
+{
+    void ProcessPayment(decimal amount);
+}
+
+public class CreditCardProcessor : IPaymentProcessor
+{
+    public void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"Processing credit card payment of {amount} amount");
+        // Implement card payment logic
+    }
+}
+
+public class PayPalProcessor : IPaymentProcessor
+{
+    public void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"Processing PayPal card payment of {amount} amount");
+        // Implement Pall payment logic
+    }
+}
+
+public class PaymentService
+{
+    private readonly IPaymentProcessor _processor;
+
+    public PaymentService(IPaymentProcessor processor)
+    {
+        _processor = processor;
+    }
+
+    public void ProcessOrderPayment(decimal amount)
+    {
+        _processor.ProcessPayment(amount);
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IPaymentProcessor creditCardProcessor = new CreditCardProcessor();
+        PaymentService paymentService = new PaymentService(creditCardProcessor);
+        paymentService.ProcessOrderPayment(100);
+        
+        IPaymentProcessor payPalProcessor = new PayPalProcessor();
+        PaymentService payPalService = new PaymentService(payPalProcessor);
+        payPalService.ProcessOrderPayment(100);
     }
 }
