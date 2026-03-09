410a732944901dab6c1ea52e2c98fe04b3023e9e
byniov1 <karol.nowak0490@protonmail.com>
2026-03-09 15:23:27 +0100

    feat: Section 10 - Sctructs - init

---
+++ Section 10 - Structs/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Section_10___Structs;
+
+public struct Point
+{
+    // public int X { get; set; }
+    // public int Y { get; set; }
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    
+    public void Display()
+    {
+        Console.WriteLine($"Point: ({X}, {Y})");
+        // Console.SetCursorPosition(X, Y);
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("P0: ");
+        Point p0 = new Point();
+        p0.Display();
+        
+        
+        Console.Write("P1: ");
+        Point p1 = new Point(1, 1);
+        p1.Display();
+       
+        
+        Console.Write("P2: ");
+        //Do tego potrzebujemy fileds nie props
+        Point p2;
+        p2.X = 10;
+        p2.Y = 20;
+        p2.Display();
+
+        //Tutaj widać że jest podawany value a nie reference
+        Console.Write("P3: ");
+        Point p3 = p1;
+        p3.X = 50;
+        p3.Y = 100;
+        p3.Display();
+        
+        
+        Console.Write("P1: ");
+        p1.Display();
+
+    }
+}
