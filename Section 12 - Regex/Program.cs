df39e9d901aee6a823373a0b8b387f16c729b8da
byniov1 <karol.nowak0490@protonmail.com>
2026-03-13 15:34:50 +0100

    feat: example how to use regex in c#

--- Section 12 - Regex/Program.cs
+++ Section 12 - Regex/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Section_12___Regex;
+﻿using System.Text.RegularExpressions;
+
+namespace Section_12___Regex;
 
 // https://learn.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string pattern = @"\d";
+        Regex regex = new Regex(pattern);
+
+        string text = "Hi there, my number is 12314";
+        
+        MatchCollection matchCollection = regex.Matches(text);
+        
+        Console.WriteLine($"{matchCollection.Count} hits found in test: {text} \n");
+        foreach (Match match in matchCollection)
+        {
+            GroupCollection groupCollection = match.Groups;
+            Console.WriteLine($"{groupCollection[0].Value} found at {groupCollection[0].Index} index");
+        }
     }
 }
