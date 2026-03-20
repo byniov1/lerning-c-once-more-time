9a742dce262b406d0b42984877517b37cf40a151
byniov1 <karol.nowak0490@protonmail.com>
2026-03-20 16:41:03 +0100

    feat: section 17 - linq - init

---
+++ Section 17 - Linq/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Section_17___Linq;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static void OddNumbers(int[] numbers)
+    {
+        IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number; 
+            // numbers.Where(n => n % 2 == 1);
+    }
+
+}
